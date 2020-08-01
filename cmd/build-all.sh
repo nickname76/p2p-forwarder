be0763a9131033413222fa17703fe1a2cf204356
nickname32 <41116859+nickname32@users.noreply.github.com>
2020-08-01 16:57:50 +0300

    Update build-all.sh

--- cmd/build-all.sh
+++ cmd/build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./build-and-zip-all-targets.sh ./tui ./tui-builds "P2P Forwarder tui"
+./build-and-zip-all-targets.sh ./tui ./tui-builds "P2P_Forwarder_tui"
 ./build-and-zip-all-targets.sh ./cli ./cli-builds "P2P_Forwarder_cli"
 # ./build-and-zip-all-targets.sh ./gui ./gui-builds "P2P Forwarder gui"
